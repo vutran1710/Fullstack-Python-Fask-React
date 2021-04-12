aa9b8a8610c31259c5ffa9701f5d75f661ad722c
vutr <me@vutr.io>
2021-04-12 22:46:33 +0700

    Add Status-API for checking writing status

--- backend/main.py
+++ backend/main.py
@@ -10,6 +10,7 @@ from models import (
     DataReport,
     GenDataAPIResponse,
     DataReportAPIResponse,
+    StatusAPIResponse,
 )
 from utils import get_dependencies
 
@@ -23,18 +24,30 @@ def generate_random_data():
     Return file-info data on success
     """
     size = convert_file_size(request.args.get("size"))
-    datagen, cache, fwriter = get_dependencies("dg", "ch", "fw")
+    datagen, cache, fwriter, status = get_dependencies("dg", "ch", "fw", "st")
     path = make_file_path()
     report = DataReport()
     data_stream = datagen.generate_randoms(hook=report.update)
 
     def write_file_async():
-        nonlocal fwriter, path, data_stream, size, report
-        file_info = fwriter.write(path, data_stream, max_size=size)
-        cache.save_data(file_info.name, report.dict())
+        nonlocal fwriter, path, data_stream, size, report, status
+        file_name = get_file_name(path)
+        status.update_status(file_name, "WAITING")
+        fwriter.write(path, data_stream, max_size=size)
+        cache.save_data(file_name, report.dict())
+        status.update_status(file_name, "FINISH")
 
     Thread(target=write_file_async).start()
     return GenDataAPIResponse(path=path, size=size).json()
+
+
+@app.route("/check-status")
+def check_file_write_status():
+    """Return writing status for a specific file"""
+    status = get_dependencies("st")
+    name = fix_file_name(request.args.get("file"))
+    stat = status.check_status(name)
+    return StatusAPIResponse(file=name, status=stat).json() if stat else {}
 
 
 @app.route("/data-report")
@@ -48,16 +61,7 @@ def get_file_report():
     cache = get_dependencies("ch")
     file_name = fix_file_name(request.args.get("file"))
     result = cache.get_data(file_name)
-    return DataReportAPIResponse(**result).json() if result else None
-
-
-@app.route("/get-all")
-def get_all_data():
-    """
-    debugging cache
-    """
-    cache = get_dependencies("ch")
-    return cache.get_all()
+    return DataReportAPIResponse(**result).json() if result else {}
 
 
 if __name__ == "__main__":
