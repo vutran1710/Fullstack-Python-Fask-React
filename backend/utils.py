66419300a242b3d0e7103e2a6d5eb0e7e885cc4c
vutr <me@vutr.io>
2021-04-12 20:18:05 +0700

    Add context-getter

---
+++ backend/utils.py
@@ -0,0 +1,19 @@
+from flask import g
+from services import Cache, FileWriter, DataGenerator
+
+
+def get_dependencies(*services):
+    """Retrive the service-instance of either
+    Cache, FileWriter or DataGenerator
+    """
+    if "ch" not in g:
+        g.ch = Cache()
+
+    if "fw" not in g:
+        g.fw = FileWriter()
+
+    if "dg" not in g:
+        g.dg = DataGenerator()
+
+    svcs = [getattr(g, s) for s in services]
+    return svcs
