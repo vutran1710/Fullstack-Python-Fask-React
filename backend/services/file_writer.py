6f108261b8f572a6edc4526662fdc2c93cbe9e0c
vutr <me@vutr.io>
2021-04-12 20:52:36 +0700

    Allow to override file-writer file-size constraint

--- backend/services/file_writer.py
+++ backend/services/file_writer.py
@@ -6,11 +6,12 @@ class FileWriter:
     def __init__(self, max_size=1024):
         self.max_size = max_size
 
-    def write(self, path: str, data_stream: Generator) -> FileInfo:
+    def write(self, path: str, data_stream: Generator, max_size=None) -> FileInfo:
+        max_size = max_size or self.max_size
         info = FileInfo(path=path, size=0)
 
         with open(path, "w+") as f:
-            while f.tell() < self.max_size:
+            while f.tell() < max_size:
                 f.write(next(data_stream))
             info.size = f.tell()
 
