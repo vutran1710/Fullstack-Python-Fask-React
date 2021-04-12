7854699b7fba423ee42374971273bc34db631032
vutr <me@vutr.io>
2021-04-12 19:17:03 +0700

    update models and file-writer class

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,6 +1,13 @@
+from typing import Optional
 from pydantic import BaseModel
+from services.logic import get_file_name
 
 
 class FileInfo(BaseModel):
-    name: str
+    name: Optional[str]
+    path: str
     size: int
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.name = get_file_name(self.path)
--- backend/services/file_writer.py
+++ backend/services/file_writer.py
@@ -3,32 +3,19 @@ from typing import Generator
 from random import choice
 from string import digits
 from models import FileInfo
+from .logic import get_file_name
 
 
 class FileWriter:
-    def __init__(self, path=None, max_size=1024):
-        """
-        Max-size for a file to write to is 1Kb
-        """
-        self.set_path(path=path)
+    def __init__(self, max_size=1024):
         self.max_size = max_size
 
-    def set_path(self, path=None):
-        if not path:
-            prefix = "static/data-"
-            suffix = "".join([choice(digits) for _ in range(4)])
-            ext = ".txt"
-            path = prefix + suffix + ext
-
-        self.path = path
-
-    def write(self, stream_generator: Generator, path=None) -> FileInfo:
-        path = path or self.path
-        info = FileInfo(name=path[8:], size=0)
+    def write(self, path: str, data_stream: Generator) -> FileInfo:
+        info = FileInfo(path=path, size=0)
 
         with open(path, "w+") as f:
             while f.tell() < self.max_size:
-                f.write(next(stream_generator))
+                f.write(next(data_stream))
             info.size = f.tell()
 
         return info
