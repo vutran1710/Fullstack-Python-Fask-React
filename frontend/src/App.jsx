6578a7d7782699bd2b382a59a32e4af6eb75ec26
vutr <me@vutr.io>
2021-04-13 20:51:58 +0700

    Update App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,56 @@
+import { pick } from 'rambda'
 import { Header, DataLink, Reporter, Button } from './components'
 import { LimitedContainer } from './components/shares'
-import './styles/App.scss'
+import { useAppStore } from './states'
 
-const App = () => (
-  <div className="App">
-    <Header />
-    <LimitedContainer>
-      <div>
-	<Button title="Generate" />
-      </div>
-      <div>
-	<DataLink />
-      </div>
-      <div>
-	<Button title="Report" />
-      </div>
-      <div>
-	<Reporter />
-      </div>
-    </LimitedContainer>
-  </div>
-)
+
+const App = () => {
+  const {
+    pendingFile,
+    needCheckFile,
+    checkingStatus,
+  } = useAppStore(pick('pendingFile,needCheckFile,checkingStatus'))
+
+  const {
+    checkStatusIntervally,
+    generateButtonClick,
+    getReportData,
+  } = useAppStore(pick('checkStatusIntervally,generateButtonClick,getReportData'))
+
+  const shouldCheckStatus = pendingFile
+	&& pendingFile === needCheckFile
+	&& !checkingStatus
+
+  if (shouldCheckStatus) checkStatusIntervally()
+
+  const canGetReport = pendingFile && !needCheckFile
+
+  const report = useAppStore(s => s.dataReport[s.latestFile])
+  const fileInfo = useAppStore(s => s.fileInfo[s.latestFile || s.pendingFile])
+
+  return (
+    <div className="App">
+      <Header />
+      <LimitedContainer>
+	<div>
+	  <Button title="Generate" handler={generateButtonClick} />
+	</div>
+	<div>
+	  <DataLink fileInfo={fileInfo} />
+	</div>
+	<div>
+	  <Button
+	    title="Report"
+	    handler={getReportData}
+	    disabled={!canGetReport}
+	  />
+	</div>
+	<div>
+	  <Reporter data={report} />
+	</div>
+      </LimitedContainer>
+    </div>
+  )
+}
 
 export default App
