8d6796a7c63a3a01a0900d27dc03c8d90821a469
vutr <me@vutr.io>
2021-04-13 16:08:56 +0700

    Add Http request service for frontend

---
+++ frontend/src/services/http.js
@@ -0,0 +1,19 @@
+import wretch from 'wretch'
+
+const apiEndpoint = process.env.REACT_APP_API_ENDPOINT
+const Request = wretch(apiEndpoint)
+
+const API = {
+  generateData: '/gen-data',
+  checkStatus: '/check-status',
+  dataReport: '/data-report',
+}
+
+const abstractRequest = endpoint => (params={}) => Request
+      .url(endpoint)
+      .query(params)
+      .get()
+
+export const generateData = abstractRequest(API.generateData)
+export const checkFileStatus = file => abstractRequest(API.checkStatus)({ file })
+export const getDataReport = file => abstractRequest(API.dataReport)({ file })
